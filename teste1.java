16ebc448211c6377ea8b32502e2b0c9edfe7e99b
gabbs <134434478+Gabriellinconn@users.noreply.github.com>
2024-03-01 19:41:27 -0300

    Add files via upload
    primeiros exercicios java

---
+++ teste1.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class teste1 {
+
+    public static void main(String[] args) {
+
+        // Scanner soma = new Scanner(System.in);
+
+        // System.out.println("número 1: ");
+        // int num1 = soma.nextInt();
+
+        // System.out.println("número 2: ");
+        // int num2 = soma.nextInt();
+
+        // System.out.println("Soma: " + (num1 + num2));
+
+        /*
+         * Scanner data = new Scanner(System.in);
+         * 
+         * System.out.println("Olá, que dia vc nasceu? ");
+         * int dia = data.nextInt();
+         * 
+         * System.out.println("Olá, que mês vc nasceu? ");
+         * int mes = data.nextInt();
+         * 
+         * System.out.println("Olá, que ano vc nasceu? ");
+         * int ano = data.nextInt();
+         * 
+         * System.out.println("Você Nasceu dia " + dia + " do mês " + mes + " Do Ano " +
+         * ano);
+         */
+
+        /*
+         * Scanner leitor = new Scanner(System.in);
+         * 
+         * System.out.printf("Olá, Qual é o seu nome? ");
+         * String nome = leitor.nextLine();
+         * 
+         * System.out.print("Bem Vindo " + nome);
+         * System.out.println("Em que ano vc nasceu?");
+         * Scanner ida = new Scanner(System.in);
+         * 
+         * int ano = ida.nextInt();
+         * 
+         * System.out.println("Então Você deve ter " + (2024 - ano) + " Anos");
+         */
+
+        /*
+         * int num = 30;
+         * float num1 = 44.33f;
+         * Boolean seila = false;
+         * String Valor = "Textinho";
+         * 
+         * float soma = num + num1;
+         * 
+         * System.out.printf("Hello World! " + +num + " Algo " + num1);
+         * System.out.printf("\nFrase " + seila + " - " + Valor);
+         * System.out.println("\nSoma:" + soma);
+         */
+        // Scanner num = new Scanner(System.in);
+        // System.out.print("Digite Número: ");
+        // int n = num.nextInt();
+        // System.out.printf("O antecessor é " + (n-1) + " e o sucessor é " + (n+1));
+
+    }
+
+}
