48a02be44ee47fc1a553ea32f9aa114c1e7aefc4
gabbs <134434478+Gabriellinconn@users.noreply.github.com>
2024-03-04 21:06:02 -0300

    Add files via upload

--- teste1.java
+++ teste1.java
@@ -61,6 +61,299 @@ public class teste1 {
         // System.out.print("Digite Número: ");
         // int n = num.nextInt();
         // System.out.printf("O antecessor é " + (n-1) + " e o sucessor é " + (n+1));
+        // Scanner leitor = new Scanner(System.in);
+        // System.out.println("Que dia voce nasceu? (dd/mm/aaaa)");
+        // String resposta = leitor.nextLine();
+
+        // String dia = resposta.substring(0, 2);
+        // String mes = resposta.substring(3, 5);
+        // String ano = resposta.substring(6, 10);
+
+        // System.out.println("Então vc nasceu no dia "+ dia + " no mês "+ mes + " no
+        // ano de "+ ano);
+
+        // Scanner num = new Scanner(System.in);
+        // System.out.println("Digite um número: ");
+        // int n = num.nextInt();
+        // if(n<10){
+        // System.out.println("O número " + n + " é menor do que 10");
+        // }
+        // else
+        // System.out.println("O número " + n + " é maior ou igual a 10");
+        // Scanner num1 = new Scanner(System.in);
+        // System.out.println("Digite um número: ");
+        // int n1 = num1.nextInt();
+
+        // Scanner num2 = new Scanner(System.in);
+        // System.out.println("Digite um número: ");
+        // int n2 = num2.nextInt();
+
+        // if(n1>n2){
+        // System.out.println("O número " +n1+ " é maior que "+ n2);
+        // }
+        // if(n1<n2){
+        // System.out.println("O número " +n2+ " é maior que "+ n1);
+        // }
+        // if(n1==n2) {
+        // System.out.println("Os números "+n1 +" e " +n2+ " são iguais");
+        // }
+        // Scanner num1 = new Scanner(System.in);
+        // System.out.println("Digite um número: ");
+        // int n1 = num1.nextInt();
+
+        // Scanner num2 = new Scanner(System.in);
+        // System.out.println("Digite um número: ");
+        // int n2 = num2.nextInt();
+
+        // if(n1>n2){
+        // System.out.println("O número " +n1+ " é maior que "+ n2);
+        // }
+        // if(n1<n2){
+        // System.out.println("O número " +n2+ " é maior que "+ n1);
+        // }
+        // if(n1==n2) {
+        // System.out.println("sequência de números é inválida");
+        // }
+        // Scanner num1 = new Scanner(System.in);
+        // System.out.println("Digite um número: ");
+        // int n1 = num1.nextInt();
+
+        // Scanner num2 = new Scanner(System.in);
+        // System.out.println("Digite um número 2: ");
+        // int n2 = num2.nextInt();
+
+        // System.out.println("Soma: "+(n1+n2)+ " Subtração "+ (n1-n2) + " Multiplicão
+        // "+ (n1*n2)+ " Divisão "+ (n1/n2));
+
+        // Scanner num1 = new Scanner(System.in);
+        // System.out.println("Digite um número: ");
+        // int n1 = num1.nextInt();
+
+        // Scanner num2 = new Scanner(System.in);
+        // System.out.println("Digite um número 2: ");
+        // int n2 = num2.nextInt();
+
+        // System.out.println("Valor do número 1 é "+n1+" Valor do número 2 é "+n2);
+
+        // int aux = n1;
+        // n1 = n2;
+        // n2 = aux;
+
+        // System.out.println("Mudança: Valor do número 1 é " +n1+ " Valor do número 2 é
+        // " +n2);
+
+        // Scanner temp = new Scanner(System.in);
+        // System.out.println("Digite uma temperatura em C° para ser convertida: ");
+        // int num = temp.nextInt();
+
+        // System.out.println("Temperatura em C° convertida em Fahrenheit é: "+((9 * num
+        // + 160) / 5)+"F°");
+
+        // Scanner num = new Scanner(System.in);
+        // System.out.println("Digite um número para saber se ele está entre 100 e 200:
+        // ");
+        // int n = num.nextInt();
+
+        // if(n>=100 && n<=200){
+        // System.out.println("O seu número está entre 100 e 200");
+
+        // }else{
+        // System.out.println("O seu número não está entre 100 e 200");
+
+        // }
+
+        // Scanner num = new Scanner(System.in);
+        // System.out.println("Digite um número");
+        // int n = num.nextInt();
+
+        // if(n>=50){
+        // System.out.println("O seu número é maior ou igual a 50");
+
+        // }else{
+        // System.out.println("O seu número é menor que 50");
+
+        // }
+        // Scanner num = new Scanner(System.in);
+        // System.out.println("Digite um número de 1 a 5");
+        // int n = num.nextInt();
+
+        // switch (n) {
+        // case 1:
+        // System.out.println("Número um");
+
+        // break;
+
+        // case 2:
+        // System.out.println("Número dois");
+
+        // break;
+
+        // case 3:
+        // System.out.println("Número tres");
+
+        // break;
+
+        // case 4:
+        // System.out.println("Número quatro");
+
+        // break;
+
+        // case 5:
+        // System.out.println("Número cinco");
+
+        // break;
+
+        // default:
+        // System.out.println("Número Inválido");
+        // break;
+        // }
+
+        // Scanner num = new Scanner(System.in);
+        // System.out.println("Digite o primeiro número: ");
+        // int n1 = num.nextInt();
+
+        // int maior = n1;
+
+        // System.out.println("Digite o segundo número: ");
+        // int n2 = num.nextInt();
+
+        // int menor = 0;
+
+        // if(maior<n2){
+        // maior = n2;
+        // menor = n1;
+        // }
+        // else{
+        // menor = n2;
+        // }
+
+        // System.out.println("Digite o terceiro número: ");
+        // int n3 = num.nextInt();
+
+        // int meio = 0;
+
+        // if(maior>n3 && n3> menor){
+
+        // meio = n3;
+        // }
+
+        // else if(n3>maior){
+        // meio = maior;
+        // maior=n3;
+        // }
+        // else{
+        // meio = menor;
+        // menor = n3;
+        // }
+
+        // System.out.println("Ordem Crescente desses números: "+menor+", "+meio+",
+        // "+maior+". ");
+
+        // Scanner mes = new Scanner(System.in);
+        // System.out.println("Digite um número de mês");
+        // int m = mes.nextInt();
+
+        // switch (m) {
+        // case 1:
+        // System.out.println("Janeiro");
+
+        // break;
+
+        // case 2:
+        // System.out.println("Fevereiro");
+
+        // break;
+
+        // case 3:
+        // System.out.println("Março");
+
+        // break;
+
+        // case 4:
+        // System.out.println("Abril");
+
+        // break;
+
+        // case 5:
+        // System.out.println("Maio");
+
+        // break;
+        // case 6:
+        // System.out.println("Junho");
+        // break;
+
+        // case 7:
+        // System.out.println("Julho");
+
+        // break;
+
+        // case 8:
+        // System.out.println("Agosto");
+
+        // break;
+
+        // case 9:
+        // System.out.println("Setembro");
+
+        // break;
+
+        // case 10:
+        // System.out.println("Outubro");
+
+        // break;
+
+        // case 11:
+        // System.out.println("Novembro");
+
+        // break;
+
+        // case 12:
+        // System.out.println("Dezembro");
+
+        // break;
+
+        // default:
+        // System.out.println("Mês Inválido");
+        // break;
+        // }
+
+        Scanner num = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        int n1 = num.nextInt();
+
+        int maior = n1;
+
+        System.out.println("Digite o segundo número: ");
+        int n2 = num.nextInt();
+
+        int menor = 0;
+
+        if (maior < n2) {
+            maior = n2;
+            menor = n1;
+        } else {
+            menor = n2;
+        }
+
+        System.out.println("Digite o terceiro número: ");
+        int n3 = num.nextInt();
+
+        int meio = 0;
+
+        if (maior > n3 && n3 > menor) {
+
+            meio = n3;
+        }
+
+        else if (n3 > maior) {
+            meio = maior;
+            maior = n3;
+        } else {
+            meio = menor;
+            menor = n3;
+        }
+
+        System.out.println("Ordem Crescente desses números: " + menor + ", " + meio + ", " + maior + ". ");
 
     }
 
